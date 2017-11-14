ba104157b2a4ec4ad08bd21e413d3a265c14b83d
李春 <lichun@500wan.com>
2017-11-14 12:46:11 +0800

    optimate

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,56 @@
 <template>
     <section class="l-full l-flex-column">
         <header class="header l-box-center">微信商城</header>
-        <section class="content"></section>
+        <section class="content">
+
+            <ul class="wrap">
+                <li class="item">
+
+                    1
+
+                </li>
+
+                <li class="item">
+
+                    2
+
+                </li>
+
+                <li class="item">
+
+                    3
+
+                </li>
+
+                <li class="item">
+
+                    4
+
+                </li>
+
+                <li class="item">
+
+                    5
+
+                </li>
+
+                <li class="item">
+
+                    6
+
+                </li>
+
+                <li class="item">
+
+                    7
+
+                </li>
+
+
+
+            </ul>
+
+        </section>
         <footer class="footer"></footer>
     </section>
 </template>
@@ -12,6 +61,19 @@
         font-size: .5rem;
         color: white;
     }
+    .wrap {
+        overflow: hidden;
+    }
+    .item {
+        width: 2.5rem;
+        float: left;
+        height: 2.5rem;
+        border: 1px solid red;
+        box-sizing: border-box;
+
+
+    }
+
 
 
 
