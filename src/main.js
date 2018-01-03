9d7562a53eee604fe623697e78922c5c191b4ade
xiezg <xiezg@500wan.com>
2018-01-03 18:09:38 +0800

    '13'

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import router from './router'
 import { sync } from 'vuex-router-sync'
 import vueTap from 'v-tap'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use( ElementUI );
 Vue.use(vueTap)
 const store = storeFactory()
 sync(store, router)
