73004158034971bb1d0fca154b2ed05102066b7c
xiezg <xiezg@500wan.com>
2018-01-04 17:00:44 +0800

    '14xtSet'

--- src/pages/home/xtManage/xtSet.vue
+++ src/pages/home/xtManage/xtSet.vue
@@ -1,18 +1,19 @@
 <template>
     <section id="xtSet">
-        <div>
-            <el-checkbox v-model="isOpenChat">用户检查</el-checkbox></br>
-            <p>
-                <span>充值检查数目 : </span>&nbsp;
-                <el-input v-model="input" placeholder="请输入数目"></el-input>&nbsp;
-                <span> 元宝，(单次充值超过此数目时触发)</span>
-            </p>
-            <p>
-                <span>充值检查数目 : </span>&nbsp;
-                <el-input  v-model="input" placeholder="请输入数目"></el-input>&nbsp;
-                <span> 元宝，(单次充值超过此数目时触发)</span>
-            </p>
-        </div>
+        <!-- 数据之后要处理对应一下 -->
+        <!--<div>-->
+            <!--<el-checkbox v-model="isOpenChat">用户检查</el-checkbox></br>-->
+            <!--<p>-->
+                <!--<span>充值检查数目 : </span>&nbsp;-->
+                <!--<el-input v-model="input" placeholder="请输入数目"></el-input>&nbsp;-->
+                <!--<span> 元宝，(单次充值超过此数目时触发)</span>-->
+            <!--</p>-->
+            <!--<p>-->
+                <!--<span>充值检查数目 : </span>&nbsp;-->
+                <!--<el-input  v-model="input" placeholder="请输入数目"></el-input>&nbsp;-->
+                <!--<span> 元宝，(单次充值超过此数目时触发)</span>-->
+            <!--</p>-->
+        <!--</div>-->
         <div>
             <p>平板租借检查：默认开启，不可设置</p>
         </div>
