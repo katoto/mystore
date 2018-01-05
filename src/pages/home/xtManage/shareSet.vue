ffc05306bcee796f6c9a0a4ae4032984fbc2153c
xiezg <xiezg@500wan.com>
2018-01-05 11:13:06 +0800

    '15'

---
+++ src/pages/home/xtManage/shareSet.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div>
+            当前分享状态：
+            <el-switch
+                v-model="value2"
+                active-color="#13ce66"
+                inactive-color="#ff4949">
+            </el-switch>
+        </div>
+        <div>
+            <p>日累计充值数目（元宝）：</p>
+            <el-select v-model="value" placeholder="请选择">
+                <el-option
+                    v-for="item in options2"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled">
+                </el-option>
+            </el-select>
+        </div>
+        <div>
+            <p>日累计充值数目（元宝）：</p>
+            <el-select v-model="value" placeholder="请选择">
+                <el-option
+                    v-for="item in options2"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled">
+                </el-option>
+            </el-select>
+        </div>
+        <hr>
+        <section>
+            <el-button>更新</el-button>
+            <el-button>重置</el-button>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                title: ''
+            }
+        },
+        watch: {},
+        methods: {},
+        computed: {},
+        mounted () {
+
+        }
+    }
+</script>
+<style>
+</style>
--- src/pages/home/xtManage/xtLog.vue
+++ src/pages/home/xtManage/xtLog.vue
@@ -1,14 +1,138 @@
 <template>
-    <div id="app">
+    <div>
+        <header>
+            <div class="block">
+                <el-date-picker
+                    v-model="value7"
+                    type="daterange"
+                    align="right"
+                    unlink-panels
+                    range-separator="至"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期"
+                    :picker-options="pickerOptions2">
+                </el-date-picker>
+            </div>
+            <div>
+                <p>查询id：</p>
+                <el-select v-model="value" placeholder="请选择">
+                    <el-option
+                        v-for="item in options2"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                        :disabled="item.disabled">
+                    </el-option>
+                </el-select>
+                <el-button type="primary">查询</el-button>
+            </div>
+        </header>
+        <section>
+            <el-table
+                :data="tableData3"
+                height="350"
+                size="small"
+                border
+                style="width: 100%">
+                <el-table-column
+                    prop="date"
+                    label="时间"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="name"
+                    label="用户账号"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="address"
+                    label="登陆IP">
+                </el-table-column>
+            </el-table>
+        </section>
     </div>
 </template>
 
 <script>
     export default {
-        data () {
+        data() {
             return {
-                title: '我是頭部'
-            }
+                pickerOptions2: {
+                    shortcuts: [{
+                        text: '最近一周',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }, {
+                        text: '最近一个月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }, {
+                        text: '最近三个月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }]
+                },
+                value7: '',
+                options2: [
+                    {
+                        value: '100万',
+                    },
+                    {
+                        value: '1000万',
+                        disabled: true
+                    },
+                    {
+                        value: '10万',
+                    },
+                    {
+                        value: '1万',
+                    },
+                    {
+                        value: '1000',
+                    }],
+                value: '10万',
+                tableData3: [{
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-08',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-06',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-07',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }]
+            };
         },
         watch: {},
         methods: {},
