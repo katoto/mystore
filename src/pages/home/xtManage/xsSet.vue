70b0e9ff5d07eae0ade9cdb8f0ead9d0c9037e74
guowf <380482989@qq.com>
2018-01-05 09:51:47 +0800

    'd'

--- src/pages/home/xtManage/xsSet.vue
+++ src/pages/home/xtManage/xsSet.vue
@@ -1,5 +1,16 @@
 <template>
     <div id="app">
+        <section id="xsSet">
+            <div class="list">
+                <h2>所有会员充值比例设置：</h2>
+                1游戏币 =<el-input v-model="xsAll" placeholder="请输入"></el-input> 元宝
+            </div>
+            <div class="list"></div>
+            <div class="list"></div>
+            <div class="list"></div>
+            <div class="list"></div>
+            <div class="list"></div>
+        </section>
     </div>
 </template>
 
@@ -7,7 +18,8 @@
     export default {
         data () {
             return {
-                title: '我是頭部'
+                title: '我是頭部',
+                xsAll:'',
             }
         },
         watch: {},
@@ -18,5 +30,13 @@
         }
     }
 </script>
-<style>
+<style scoped>
+    #xsSet .list{
+        border-bottom:1px solid #303133;
+        color: #303133;
+    }
+    #xsSet h2{
+        font-size: 16px;
+        font-weight:bold;
+    }
 </style>
