10f4704e4a5f7c1cfc2d5d61e6fc6a2012939e2a
xiezg <xiezg@500wan.com>
2018-01-05 09:53:12 +0800

    '15'

--- src/pages/home/xtManage/userIP.vue
+++ src/pages/home/xtManage/userIP.vue
@@ -1,5 +1,42 @@
 <template>
-    <div id="app">
+    <div>
+        <div>
+            当前分享状态：
+            <el-switch
+                v-model="value2"
+                active-color="#13ce66"
+                inactive-color="#ff4949">
+            </el-switch>
+        </div>
+        <div>
+            <p>日累计充值数目（元宝）：</p>
+            <el-select v-model="value" placeholder="请选择">
+                <el-option
+                    v-for="item in options2"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled">
+                </el-option>
+            </el-select>
+        </div>
+        <div>
+            <p>日累计充值数目（元宝）：</p>
+            <el-select v-model="value" placeholder="请选择">
+                <el-option
+                    v-for="item in options2"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled">
+                </el-option>
+            </el-select>
+        </div>
+        <hr>
+        <section>
+            <el-button>更新</el-button>
+            <el-button>重置</el-button>
+        </section>
     </div>
 </template>
 
@@ -7,7 +44,7 @@
     export default {
         data () {
             return {
-                title: '我是頭部'
+                title: ''
             }
         },
         watch: {},
