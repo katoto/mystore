220a93e300fa4764812c363cc123bd716b3058e5
guowf <380482989@qq.com>
2018-01-10 15:09:15 +0800

    'd'

--- src/pages/home/tgyManage/tgyManage.vue
+++ src/pages/home/tgyManage/tgyManage.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <h2>tgyManage</h2>
+        <div class="overflow">
+            <h2 class="fl">推广员管理</h2>
+            <p class="fr red">当前可处理的订单数：充值申请0个，兑奖申请0个</p>
+        </div>
         <router-view></router-view>
     </div>
 </template>
@@ -19,5 +22,22 @@
         }
     }
 </script>
-<style>
+<style scoped>
+    h2.fl{
+        font-size:16px;
+        color: #409EFF;
+    }
+    .fl{
+        float: left;
+    }
+    .fr{
+        float: right;
+    }
+    .overflow{
+        overflow: hidden;
+    }
+    .red{
+        color: red;
+    }
+
 </style>
