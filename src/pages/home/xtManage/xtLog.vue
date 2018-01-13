7d83cb3376854b871d7aa0375dd0450760af350c
turbo <zgxie@500wan>
2018-01-13 16:15:36 +0800

    '113'

--- src/pages/home/xtManage/xtLog.vue
+++ src/pages/home/xtManage/xtLog.vue
@@ -19,10 +19,10 @@
             <span class="xtSpan">用户账号：</span>
             <el-select class="checkID xtSel" size="small" v-model="xtUserSel" placeholder="请选择">
                 <el-option
-                    v-for="item in options2"
+                    v-for="item in adminList"
                     :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
+                    :label="item.username"
+                    :value="item.id"
                     :disabled="item.disabled">
                 </el-option>
             </el-select>
@@ -87,9 +87,9 @@
                     }],
                 xtInpVal: '',
                 pickerOptions: {
-//                    disabledDate(time) {
-//                        return time.getTime() > Date.now() - 8.64e7;
-//                    },
+                //                    disabledDate(time) {
+                //                        return time.getTime() > Date.now() - 8.64e7;
+                //                    },
                     shortcuts: [{
                         text: '最近一周',
                         onClick (picker) {
@@ -117,34 +117,22 @@
                     }]
                 },
                 xtLogTime: '',
+                xtStartTime:null,
+                xtEndTime:null,
 
-                options2: [
-                    {
-                        value: '100万'
-                    },
-                    {
-                        value: '1000万',
-                        disabled: true
-                    },
-                    {
-                        value: '10万'
-                    },
-                    {
-                        value: '1万'
-                    },
-                    {
-                        value: '1000'
-                    }],
-                xtUserSel: '10万'
+                adminList: [],
+                xtUserSel: ''
 
             }
         },
         watch: {},
         methods: {
-            logTimeChange(val){
-                console.log( this.format( val[0] ) );
-                console.log( this.format( val[1] ) );
-//                取到值
+            logTimeChange (val) {
+                console.log(this.format(val[0]))
+                console.log(this.format(val[1]))
+                //                取到值
+                this.xtStartTime = this.format(val[0]);
+                this.xtEndTime = this.format(val[1]);
             },
             format (time, format = 'yyyy-MM-dd') {
                 let t = new Date(time)
@@ -153,71 +141,102 @@
                 }
                 return format.replace(/yyyy|MM|dd|HH|mm|ss/g, function (a) {
                     switch (a) {
-                        case 'yyyy':
-                            return tf(t.getFullYear())
-                        case 'MM':
-                            return tf(t.getMonth() + 1)
-                        case 'mm':
-                            return tf(t.getMinutes())
-                        case 'dd':
-                            return tf(t.getDate())
-                        case 'HH':
-                            return tf(t.getHours())
-                        case 'ss':
-                            return tf(t.getSeconds())
+                    case 'yyyy':
+                        return tf(t.getFullYear())
+                    case 'MM':
+                        return tf(t.getMonth() + 1)
+                    case 'mm':
+                        return tf(t.getMinutes())
+                    case 'dd':
+                        return tf(t.getDate())
+                    case 'HH':
+                        return tf(t.getHours())
+                    case 'ss':
+                        return tf(t.getSeconds())
                     }
                 })
             },
-            clickPage (size) {
+            async clickPage (size) {
                 // 分页  请求数据 ，更新数据
-                console.log(size)
+                console.log(size);
+                let result = null;
+                if( !this.xtStartTime || !this.xtEndTime || !this.xtUserSel  ){
+                    result = await this.$store.dispatch(actionTypes.getXtLog, { userId: -1, starttime: '2018-01-01', endtime: this.format( new Date() ) ,pageNumber:size})
+                    console.log('全部分页')
+                    console.log(result)
+
+                }else{
+                    result = await this.$store.dispatch(actionTypes.getXtLog, { userId: this.xtUserSel  , starttime:this.xtStartTime , endtime:this.xtEndTime ,pageNumber:size})
+                    console.log('选择分页')
+                    console.log(result)
+                }
+                if (result && result.list) {
+                    let copyList = result.list
+                    this.xtLogList = copyList
+                    // 处理页码
+                    this.totalCount = result.totalCount,
+                        this.pageNumber = result.pageNumber,
+                        this.pageSize = result.pageSize
+                }
             },
-            getMsg(){
-                console.log('getMsg');
-                console.log( this.xtLogTime )
+            async getMsg () {
+                if( !this.xtStartTime || !this.xtEndTime ){
+                    this.$message({
+                        message: '请选择查询时间',
+                        type: 'error',
+                        duration: 1200
+                    })
+                    return false;
+                }
+                if( !this.xtUserSel  ){
+                    this.$message({
+                        message: '请选择查询用户',
+                        type: 'error',
+                        duration: 1200
+                    })
+                    return false;
+                }
+                console.log(this.xtStartTime)
+                console.log(this.xtEndTime)
+                console.log( this.xtUserSel );
+//                loading
+                let result = await this.$store.dispatch(actionTypes.getXtLog, { userId: this.xtUserSel, starttime: this.xtStartTime, endtime: this.xtEndTime , pageNumber:1})
+                console.log('查询')
+                console.log(result)
+                if (result && result.list) {
+                    let copyList = result.list
+                    this.xtLogList = copyList
+                    // 处理页码
+                    this.totalCount = result.totalCount,
+                    this.pageNumber = result.pageNumber,
+                    this.pageSize = result.pageSize
+                }
             }
         },
         computed: {},
         async mounted () {
-            let copyResult = {'args': [{'list': [
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:33:59'},
-                {'content': '退出管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:30:43'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:30:39'},
-                {'content': '退出管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:23:07'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:22:10'},
-                {'content': '退出管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:21:43'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:19:47'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:19:45'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:19:41'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:19:16'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:18:40'},
-                {'content': '退出管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:17:10'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:16:35'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:15:54'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:12:17'},
-                {'content': '登陆管理后台', 'authority': '超级管理员', 'admin': 'admin', 'datetime': '2018-01-08 15:12:16'}
-            ],
-            'order': 'desc',
-            'orderBy': '',
-            'pageCount': 31,
-            'pageNumber': 5,
-            'pageSize': 16,
-            'totalCount': 491}],
-            'method': 'getSystemLog'
+            // 获取管理员列表  第一个参数代表查询的用户
+            let getManageList = await this.$store.dispatch( actionTypes.adminList , [] ) ;
+            if(getManageList && getManageList.length>=0){
+                getManageList.forEach(( item ,index )=>{
+                    if( item.id && item.username ){
+                        this.adminList.push({  username:item.username ,id: item.id })
+                    }
+                })
+            }else{
+              console.error( 'adminList error at xtLog' )
             }
-            console.log(copyResult)
 
-            if (copyResult.args[0].list || 1) {
-                let copyList = copyResult.args[0].list
+            let result = await this.$store.dispatch(actionTypes.getXtLog, { userId: -1, starttime: '2018-01-01', endtime: this.format( new Date() ),pageNumber:1})
+            console.log(result)
+            if (result && result.list) {
+                let copyList = result.list
                 this.xtLogList = copyList
                 // 处理页码
-                this.totalCount = copyResult.args[0].totalCount,
-                this.pageNumber = copyResult.args[0].pageNumber,
-                this.pageSize = copyResult.args[0].pageSize
+                this.totalCount = result.totalCount,
+                this.pageNumber = result.pageNumber,
+                this.pageSize = result.pageSize
             }
-            let result = await this.$store.dispatch(actionTypes.getXtLog, { firstParam: -1, starttime: '2018-01-01', endtime: '2018-01-08'})
-            console.log(result)
-            console.log(result)
         }
     }
 </script>
@@ -248,10 +267,8 @@
     header button{
         float: left;
     }
-
-
     .checkID{
-        width: 60%;
+        width: 80%;
     }
     .el-date-editor--daterange.el-input, .el-date-editor--daterange.el-input__inner, .el-date-editor--timerange.el-input, .el-date-editor--timerange.el-input__inner{
         width: 95%;
